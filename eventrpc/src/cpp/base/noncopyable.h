c957095a85026cdcc2247848f6f9d1e77fc68ca0
lichuang1982 <lichuang1982@b654511f-0460-f44d-d821-1d58b4c5bbf0>
2010-10-24 04:21:10 +0000

    remove src dir

--- eventrpc/src/cpp/base/noncopyable.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __EVENTRPC_NONCOPYABLE_H__
-#define __EVENTRPC_NONCOPYABLE_H__
-
-#define DISALLOW_EVIL_CONSTRUCTOR(Type) \
-  Type(const Type&);                    \
-  void operator=(const Type&)
-
-#endif  //  __EVENTRPC_NONCOPYABLE_H__
